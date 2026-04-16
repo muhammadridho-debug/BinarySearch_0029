e57666a6d8777774ccdcb5b356a8def0418a30a8
muhammadridho-debug <idomuhammad11@gmail.com>
2026-04-16 15:49:24 +0700

    implementasi fungsi input dengan validasi batas array

--- binarysearch.cpp
+++ binarysearch.cpp
@@ -4,3 +4,32 @@ using namespace std;
 int elemen[10];
 int npanjang;
 int x;
+
+void input()
+{
+    while (true)
+    {
+        cout << "Masukkan banyaknya elemen pada array (maksimal 10): ";
+        cin >> npanjang;
+
+        if (npanjang <= 10)
+        {
+            break;
+        }
+        else
+        {
+            cout << "\n[!] Jumlah elemen tidak boleh lebih dari 10. Silahkan Coba lagi.\n";
+        }
+    }
+
+    cout << "\n=======================================\n";
+    cout << "          Masukkan Elemen Array          \n";
+    cout << "\n=======================================\n";
+
+    for (int i = 0; i < npanjang; i++)
+    {
+        cout << "Data ke-" << (i + 1) << " = ";
+        cin >> elemen[i];
+    }
+    
+}
