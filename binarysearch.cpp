aff135692b22c795e2f57a5918f93cca4dc633e0
muhammadridho-debug <idomuhammad11@gmail.com>
2026-04-16 15:53:37 +0700

    implementasi logika awal Binary Search

--- binarysearch.cpp
+++ binarysearch.cpp
@@ -68,3 +68,16 @@ void display()
     }
     cout << endl;
 }
+
+void binarySearch()
+{
+    cout << "\n=====================================\n";
+    cout << "        Pencarian Binary Search       \n";
+    cout << "======================================\n";
+
+    cout << "Masukkan elemen yang ingin dicari: ";
+    cin >> x;
+
+    int low = 0;
+    int high = npanjang - 1;
+}
